a21b1a96524e755132381b94a195225d883e7743
Kate Evdochenko <antonovakatitosha@gmail.com>
2024-11-19 15:00:58 +0100

    Initial Commit

---
+++ ToDo List/ToDo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_ListApp.swift
+//  ToDo List
+//
+//  Created by Kate Evdochenko on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
